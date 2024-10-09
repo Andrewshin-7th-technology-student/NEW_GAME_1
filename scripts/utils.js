1a86e07fde0cbb3230a276cca450e3398166ed57
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-09 14:44:51 -0700

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,6 @@
 // Utility function to generate a random integer
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Export the randomInt function
