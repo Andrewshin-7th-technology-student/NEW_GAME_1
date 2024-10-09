01ea06e77617e87ad575dd1b0ac37b76911e3505
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-09 14:44:27 -0700

    Update level.js

--- scripts/level.js
+++ scripts/level.js
@@ -1,29 +1,29 @@
 // Level logic
 function createLevel(levelData) {
-  const level = {
-    treasures: [],
-    obstacles: [],
-  };
+	const level = {
+		powerUps: [],
+		obstacles: [],
+	};
 
-  // Create treasures
-  for (let i = 0; i < levelData.treasures.length; i++) {
-    const treasure = levelData.treasures[i];
-    level.treasures.push({
-      x: treasure.x,
-      y: treasure.y,
-    });
-  }
+	// Create power-ups
+	for (let i = 0; i < levelData.powerUps.length; i++) {
+		const powerUp = levelData.powerUps[i];
+		level.powerUps.push({
+			x: powerUp.x,
+			y: powerUp.y,
+		});
+	}
 
-  // Create obstacles
-  for (let i = 0; i < levelData.obstacles.length; i++) {
-    const obstacle = levelData.obstacles[i];
-    level.obstacles.push({
-      x: obstacle.x,
-      y: obstacle.y,
-    });
-  }
+	// Create obstacles
+	for (let i = 0; i < levelData.obstacles.length; i++) {
+		const obstacle = levelData.obstacles[i];
+		level.obstacles.push({
+			x: obstacle.x,
+			y: obstacle.y,
+		});
+	}
 
-  return level;
+	return level;
 }
 
 // Export the createLevel function
